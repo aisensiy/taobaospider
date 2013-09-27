3831731ee0fb2c8f3da9d1db268d17630e71a0ce
Xiaojiang Shen <xiaojiang@siteshen.com>
2013-09-27 12:32:25 +0800

    Fix sys.path

--- spider/config/settings.py
+++ spider/config/settings.py
@@ -3,7 +3,7 @@ import yaml
 
 # ROOT/settings.py
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-CONFIG_ROOT = os.path.join(STATIC_PATH, 'config')
+CONFIG_ROOT = os.path.join(PROJECT_ROOT, 'config')
 DB_CONFIG_FILE = os.path.join(CONFIG_ROOT, 'database.yml')
 
 DB_CONFIG = yaml.load(open(DB_CONFIG_FILE).read())['development']
