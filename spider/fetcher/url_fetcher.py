152f822dee7635471d1cb95fe979cc429cfa40b3
aisensiy <aisensiy@gmail.com>
2013-09-30 16:18:37 +0800

    bug fix for fetchall

--- spider/fetcher/url_fetcher.py
+++ spider/fetcher/url_fetcher.py
@@ -94,7 +94,7 @@ class UrlHandler:
     Fetch many rows with one column
     """
     global skip
-    rows = self.conn.fetchall("select url from taobao limit %d offset %d" % (limit, skip))
+    rows = self.conn.fetchall("select url from taobao limit %s offset %s", (limit, skip))
     rows = set(rows)
     if not len(rows): return
     skip += limit
