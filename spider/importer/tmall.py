9d6f709c29362b2c55ba3179ce8f627fdfe04473
aisensiy <aisensiy@gmail.com>
2013-10-10 12:59:34 +0800

    add tmall importer

---
+++ spider/importer/tmall.py
@@ -0,0 +1,40 @@
+from csv_importer import Table
+import sys
+
+class Tmall(Table):
+  def __init__(self, tablename, conn):
+    super(Tmall, self).__init__(tablename, conn)
+
+  def getfields(self):
+    fields = [
+      ('uid', 0, str),
+      ('ip', 1, str),
+      ('agent', 2, str),
+      ('url', 5, str),
+      ('site', 6, str),
+      ('domain', 7, str),
+      ('referurl', 8, str),
+      ('date', 11, str, 'datetime'),
+      ('staytime', 12, int),
+      ('url_kw', 15, str),
+      ('refer_kw', 16, str)
+    ]
+
+    return super(Tmall, self).getfields(fields)
+
+
+def import_tmall(filename):
+  from config.settings import *
+  from util.db import MySQL as DB
+  conn = DB(DB_CONFIG)
+  tmall = Tmall('tmall', conn)
+  tmall.create_and_insert(filename, batch=600)
+  conn.close()
+
+if __name__ == '__main__':
+  import time
+  print "[INFO] Start at:", time.ctime()
+  for filename in sys.argv[1:]:
+    import_tmall(filename)
+  print "[INFO] Finis at:", time.ctime()
+
